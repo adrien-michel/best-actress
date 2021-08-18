7db9dfa940e727f595310dfd8795efc1a7ed12f6
abdomikhael <adrien.michel.dev@gmail.com>
2021-08-18 11:51:57 +0200

    add-buttons

--- script.js
+++ script.js
@@ -11,17 +11,21 @@ $(document).ready(function ($) {
     bindEvents = function () {
       $mainMenuItems.children(".images").click(function () {
         var newIndex = $(this).parent().index();
-        var $item = $mainMenuItems.eq(newIndex);
-        if ($openedIndex === newIndex) {
-          animateItem($item, false, 250);
-          $openedIndex = -1;
-        } else {
-          if (validIndex(newIndex)) {
-            animateItem($mainMenuItems.eq($openedIndex), false, 250);
-            $openedIndex = newIndex;
-            animateItem($item, true, 250);
-          }
+        checkAnimateItem(newIndex);
+      });
+
+      $(".button").hover(
+        function () {
+          $(this).addClass("hovered");
+        },
+        function () {
+          $(this).removeClass("hovered");
         }
+      );
+      $(".button").click(function () {
+        var newIndex = $(this).index();
+        $item = $mainMenuItems.eq(newIndex);
+        checkAnimateItem(newIndex);
       });
     },
     validIndex = function (indexToCheck) {
@@ -33,6 +37,18 @@ $(document).ready(function ($) {
       var colorImageParam = toOpen ? { left: "0px" } : { left: "140px" };
       $colorImage.animate(colorImageParam, speed);
       $item.animate(itemParam, speed);
+    },
+    checkAnimateItem = function (indexToCheckAndAnimate) {
+      if ($openedIndex === indexToCheckAndAnimate) {
+        animateItem($mainMenuItems.eq(indexToCheckAndAnimate), false, 250);
+        $openedIndex = -1;
+      } else {
+        if (validIndex(indexToCheckAndAnimate)) {
+          animateItem($mainMenuItems.eq($openedIndex), false, 250);
+          $openedIndex = indexToCheckAndAnimate;
+          animateItem($mainMenuItems.eq($openedIndex), true, 250);
+        }
+      }
     };
   init();
 });
